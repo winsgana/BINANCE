6ce796df999da53e7c99af48077fd054d0affb39
winsgana <winsganabet@gmail.com>
2025-03-07 03:48:23 -0400

    Update config.php

--- utils/config.php
+++ utils/config.php
@@ -1,6 +1,6 @@
 <?php
 // Configuración global
-define('TELEGRAM_TOKEN', '7649868783:AAF-aTgEuA2o7q2jaXGJ5awrysEy04hgJl4');
+define('TELEGRAM_TOKEN', '7768625990:AAGd7poAx9VCb8zxSoVS01bWI-5NVY0v0CY');
 define('TELEGRAM_CHAT_ID', '-4757550811');
 define('SMS_API_KEY', '6d32dd80bef8d29e2652d9c68148193d1ff229c248e8f731');
 ?>
